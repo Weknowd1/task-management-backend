a98e16d03559a62a130a0289c47632cba99db6b7
vinod <weknowd1@gmail.com>
2025-12-23 19:02:08 +0530

    login api with autentication added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,9 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// hash password in register
+// compare + token in login
 
 exports.register = async (req, res) => {
   const { email, password } = req.body;
